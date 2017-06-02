6d99df6fba8fa4b8ee2980a472ff7a55c51ed19b
Yang Liu <lykavin@hotmail.com>
2017-06-02 13:21:03 +0800

    optimized srCreateOrder()

--- Personal/Yang/CreateOrder.sql
+++ Personal/Yang/CreateOrder.sql
@@ -1,42 +1,34 @@
-DELIMITER $$
-DROP FUNCTION IF EXISTS srCheckSlot$$
-CREATE FUNCTION srCheckSlot(_slotId INT)
-	RETURNS INT
-    -- 1 if ok,
-    -- -1 if slot is reserved,
-    -- -2 if slot is unusable
-	-- -3 if slot not found
-BEGIN
-	DECLARE retVal INT;
-    DECLARE oldOrderId int;
-    DECLARE oldIsUsable bool;
-
-    -- put the slot info into variables
-	SELECT orderId, isUsable
-    INTO oldOrderId, oldIsUsable
-	FROM Slot
-	WHERE Slot.slotId = _slotId;
-
-    -- check if we can find the slot with slotId
-    IF oldIsUsable = null THEN RETURN -3;
-    END IF;
-	-- check if room is usable
-    IF oldIsUsable = false THEN RETURN -2;
-    END IF;
-    -- check if the slot is not reserved
-    IF oldOrderId IS NOT NULL THEN RETURN -1;
-    END IF;
-
-    -- now we could be sure that the slot could be reserved
-    -- UPDATE Slot SET Slot.orderId = _orderId
-    -- WHERE Slot.slotId = _slotId;
-	RETURN 1;
-
-END;
-$$
-DELIMITER ;
+-- Author: Yang Liu
+-- Email: lykavin@hotmail.com
 
 DELIMITER $$
+-- DROP FUNCTION IF EXISTS srCheckSlot$$
+-- CREATE FUNCTION srCheckSlot(_isUsable bool)
+-- 	RETURNS INT
+--     -- 1 if ok,
+--     -- -1 if slot is reserved,
+--     -- -2 if slot is unusable
+-- 	-- -3 if slot not found
+-- BEGIN
+-- 	DECLARE retVal INT;
+--
+--     -- check if we can find the slot with slotId
+--     IF _isUsable = null THEN RETURN -3;
+--     END IF;
+-- 	-- check if room is usable
+--     IF _isUsable = false THEN RETURN -2;
+--     END IF;
+--     -- check if the slot is not reserved
+--     IF _isUsable IS NOT NULL THEN RETURN -1;
+--     END IF;
+--
+--     -- now we could be sure that the slot could be reserved
+--     -- UPDATE Slot SET Slot.orderId = _orderId
+--     -- WHERE Slot.slotId = _slotId;
+-- 	RETURN 1;
+--
+-- END$$
+
 DROP PROCEDURE IF EXISTS srCreateOrder$$
 -- CAUTION: make sure this proceudre is executed within a transaction
 CREATE PROCEDURE srCreateOrder(
@@ -53,10 +45,10 @@ CREATE PROCEDURE srCreateOrder(
     )
 srCreateOrderCursor:
 BEGIN
+
 	DECLARE slotRetVal INT;
-    DECLARE slotCount INT;
+    -- DECLARE slotCount INT;
     DECLARE slotCursor INT;
-    DECLARE tempSlotId INT;
 
 	DECLARE orderStartTime DATETIME;
 	DECLARE orderEndTime DATETIME;
@@ -67,71 +59,56 @@ BEGIN
     DECLARE newOrderId INT;
     DECLARE orderPwd INT;
 
-	-- handler for general sqlexception error(commented)
---     DECLARE EXIT HANDLER FOR SQLEXCEPTION
--- 		BEGIN
--- 			SET _outOrderId = -4;
--- 			ROLLBACK;
--- 		END;
-
 	DROP TEMPORARY TABLE IF EXISTS SlotList;
+
 	CREATE TEMPORARY TABLE SlotList(
 		slotId INT,
 		roomId INT,
 		startTime DATETIME,
-		endTime DATETIME
+		endTime DATETIME,
+		isUsable BOOLEAN,
+		orderId INT
 	);
 
-	-- parse _slotIdStr, store slotIds in SlotList
-    INSERT INTO SlotList(slotId, roomId, startTime, endTime)
-        SELECT Slot.slotId, Slot.roomId, Slot.startTime, Slot.endTime
-        FROM Slot
-        WHERE FIND_IN_SET(Slot.slotId, _slotIdStr);
-    -- set slot count
-    SELECT COUNT(*) FROM SlotList INTO slotCount;
-	-- if we cannot find any slot in the slot array...
-	IF slotCount = 0 THEN
+	INSERT INTO SlotList(slotId, roomId, startTime, endTime, isUsable, orderId)
+		SELECT Slot.slotId, Slot.roomId, Slot.startTime,
+		       Slot.endTime, Slot.isUsable, Slot.orderId
+		FROM Slot
+		WHERE FIND_IN_SET(Slot.slotId, _slotIdStr);
+
+	IF NOT EXISTS (SELECT NULL FROM SlotList) THEN
+		-- cannot parse _slotIdStr OR cannot find slotId in Slot
 		SET _outOrderId = -3;
 		LEAVE srCreateOrderCursor;
 	END IF;
-	-- reset slot cursor
-    SET slotCursor = 0;
 
-    -- check if every slot is reservable
-    SET slotsReservable = true;
-    checkSlotLoop:
-    WHILE slotCursor < slotCount DO
-        -- get next tempSlotId in list
-        SELECT SlotList.slotId INTO tempSlotId FROM SlotList
-        ORDER BY SlotList.slotId LIMIT slotCursor, 1;
-        -- check this slot
-        SET slotRetVal = srCheckSlot(tempSlotId);
-        -- if a slot is not reservable
-        IF slotRetVal < 0 THEN
-            SET slotsReservable = false;
-            LEAVE checkSlotLoop;
-        END IF;
-        SET slotCursor = slotCursor + 1;
-    END WHILE;
+	IF EXISTS
+		(SELECT NULL
+		 FROM SlotList
+		 WHERE SlotList.isUsable != true OR SlotList.orderId IS NOT NULL)
+	THEN
+		-- some slot is not
+		SET _outOrderId = -1;
+		LEAVE srCreateOrderCursor;
+	END IF;
 
-    -- if a slot is not reservable, set out parameter and leave procedure
-    IF slotsReservable = false THEN
-        SET _outOrderId = slotRetVal;
-        LEAVE srCreateOrderCursor;
-    END IF;
 
 	-- we could confirm all slots are reservable now.
     -- prepare order:
 	SET orderStartTime =
-		(SELECT DISTINCT MIN(startTime) FROM SlotList);
+		(SELECT MIN(startTime) FROM SlotList LIMIT 1);
 	SET orderEndTime =
-	 	(SELECT DISTINCT MAX(endTime) FROM SlotList);
+	 	(SELECT MAX(endTime) FROM SlotList LIMIT 1);
 	SET orderRoomId =
 		(SELECT roomId FROM SlotList LIMIT 1);
 
     -- generate randomized 6-digit order password:
     -- [100000, 999999]
-    SET orderPwd = FLOOR(RAND()*(999999 - 100000 + 1)) + 100000;
+    -- SET orderPwd = FLOOR(RAND()*(999999 - 100000 + 1)) + 100000;
+
+    -- for simplicity of testing, generate 0/1 password instead
+    SET orderPwd = FLOOR(RAND()*2);
+
     -- create order (default status value is 0)
     -- and store its order id
     INSERT INTO
@@ -141,26 +118,12 @@ BEGIN
 			   orderStartTime, orderEndTime, orderRoomId);
     SET newOrderId = LAST_INSERT_ID();
 
-	-- reset slot cursor
-    SET slotCursor = 0;
-    -- iterate through the SlotList set again and modify slots
-    WHILE slotCursor < slotCount DO
-        -- get next tempSlotId in list
-        SELECT SlotList.slotId
-        INTO tempSlotId
-        FROM SlotList
-        ORDER BY SlotList.slotId
-        LIMIT slotCursor, 1;
-        -- try to modify(reserve) this slot
-		UPDATE Slot SET Slot.orderId = newOrderId
-		WHERE Slot.slotId = tempSlotId;
+    -- set orderId for corresponding slots in Slot TABLE
+	UPDATE Slot SET Slot.orderId = newOrderId
+	WHERE Slot.SlotId IN (SELECT SlotList.SlotId FROM SlotList);
 
-        SET slotCursor = slotCursor + 1;
-    END WHILE;
 
 	INSERT INTO asMember(orderId, userId) VALUES(newOrderId, _applicantId);
     SET _outOrderId = newOrderId;
 
-END
-$$
-DELIMITER ;
+END$$
